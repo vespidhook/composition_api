02ac633563cf874305534ea65b644b393b19afd5
Bruno Alves <vespidhook@gmail.com>
2023-03-16 00:16:00 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Composition API</h1>
+  <b>Name:</b> {{ user.name }} <br />
+  <b>Age:</b> {{ user.age }} <br />
+  <br><br>
+
+  <input
+    v-model="user.name"
+    type="text"
+  />
+  <button @click="changeName">Alterar nome</button>
+
+  <br><br>
+  <b>Product:</b> {{ product }} <br>
+  <input
+      v-model="product"
+      type="text"
+  />
+  <button @click="changeProduct">Alterar nome do produto</button>
+
+</template>
+
+<script>
+import {ref} from 'vue'
+  export default {
+    setup() {
+      const product = ref('Iphone')
+      const user = ref({
+        name: 'John',
+        age: 30
+      })
+
+      function changeName() {
+        user.value.name = 'Bruno Alves'
+      }
+
+      function changeProduct() {
+        product.value = 'Samsung'
+      }
+
+      return {
+        user,
+        changeName,
+        product,
+        changeProduct
+      }
+    }
+  }
+</script>
