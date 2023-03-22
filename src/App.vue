243589343b32f919d373cde5b65251d4bcbee8fb
Bruno Alves <vespidhook@gmail.com>
2023-03-21 23:56:20 -0300

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,20 @@
 <template>
   <h1>Composition API</h1>
-  {{ todoId }} <br>
-  <input
-    v-model="todoId"
-    type="text"
+  <User
+      :user="user"
+      @update="updateUser"
   />
-
 </template>
 
-<script>
-import { ref, watch, watchEffect } from 'vue'
+<script setup>
+  import User from "./components/User.vue";
+  import {ref} from "vue";
 
-  export default {
-    setup() {
-      const todoId = ref(1);
+  const user = ref({
+    first_name: 'John',
+  })
 
-      // watch(todoId, (vl) => {
-      //   console.log('todo', vl);
-      // })
-
-      watchEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/todos/${todoId.value}`)
-          .then(response => response.json())
-          .then(json => console.log(json))
-      })
-
-      return {
-        todoId
-      }
-    }
+  function updateUser() {
+    console.log('updateUser')
   }
 </script>
---
+++ src/components/User.vue
@@ -0,0 +1,29 @@
+<template>
+  <pre>
+    {{ user }}
+  </pre>
+  <button @click="emits('update')">Update</button>
+</template>
+
+<!--<script>-->
+<!--  export default {-->
+<!--    emits: ['update'],-->
+<!--    props: {-->
+<!--      user: Object-->
+<!--    },-->
+<!--    setup(props, { emit }) {-->
+<!--      emit('update')-->
+<!--    }-->
+<!--  }-->
+<!--</script>-->
+
+<script setup>
+  const emits = defineEmits(['update']);
+  const props = defineProps({
+    user: Object
+  })
+
+  emits('update')
+
+  console.log(props.user)
+</script>
